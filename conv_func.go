58390a37b72394d92cf1ae753a50e694b2f30398
injoyai <1113655791@qq.com>
2025-05-29 10:44:44 +0800

    增加函数Range,试用

--- conv_func.go
+++ conv_func.go
@@ -15,3 +15,18 @@ func Select[T any](b bool, v1, v2 T) T {
 	}
 	return v2
 }
+
+// Range 限制范围,设置默认值,组合函数,conv.Range(1,1,10,2)
+func Range[T Comparable](_default T, min, max T, vals ...T) T {
+	val := Default(_default, vals...)
+	if min > max {
+		min, max = max, min
+	}
+	if val < min {
+		return min
+	}
+	if val > max {
+		return max
+	}
+	return val
+}

6ed4f17bb8531b219ff0e6f4004aa39bbdcb4bd2
injoyai <1113655791@qq.com>
2025-05-29 10:45:25 +0800

    增加接口Comparable,同1.21版本的cmp.Ordered

--- conv_interface.go
+++ conv_interface.go
@@ -52,3 +52,10 @@ type apiBool interface {
 type apiCopy interface {
 	Copy() any
 }
+
+type Comparable interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64 |
+		string
+}

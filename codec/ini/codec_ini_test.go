8d31a16be75d3d6562902e0ffed574ff4f3465c0
injoyai <1113655791@qq.com>
2026-06-23 10:09:42 +0800

    优化

--- codec/ini/codec_ini_test.go
+++ codec/ini/codec_ini_test.go
@@ -6,48 +6,24 @@ import (
 )
 
 func TestIni_Marshal(t *testing.T) {
-	{
-		i := Ini{}
-		bs, err := i.Marshal(map[string]map[string]string{
-			"a": {
-				"b": "c",
-				"f": "g",
-			},
-			"b": {
-				"b": "c",
-				"f": "g",
-			},
-			"": {
-				"d": "e",
-				"f": "g",
-			},
-		})
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		fmt.Println(string(bs))
+	i := Ini{}
+	bs, err := i.Marshal(map[string]map[string]string{
+		"a": {
+			"b": "c",
+			"f": "g",
+		},
+		"b": {
+			"b": "c",
+			"f": "g",
+		},
+		"": {
+			"d": "e",
+			"f": "g",
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
 	}
-	{
-		i := Ini{}
-		bs, err := i.Marshal(map[string]any{
-			"a": map[string]any{
-				"b": "c",
-				"f": "g",
-			},
-			"b": map[string]any{
-				"b": "c",
-				"f": "g",
-			},
-			"": map[string]any{
-				"d": 1,
-				"f": 2,
-			},
-		})
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		fmt.Println(string(bs))
-	}
+	fmt.Println(string(bs))
 }
